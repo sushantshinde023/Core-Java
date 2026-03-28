137a5134bfbbfaacb10ebc4f6f6e4b3ab2c5b62c
sushantshinde023 <sushantshinde023@gmail.com>
2026-03-28 16:34:17 +0530

    note

--- DesignPatterns/src/com/sushant/structural/adapter/pattern/WordDocumentClassAdapter.java
+++ DesignPatterns/src/com/sushant/structural/adapter/pattern/WordDocumentClassAdapter.java
@@ -1,5 +1,11 @@
 package com.sushant.structural.adapter.pattern;
 
+
+/*
+ * With this class adapter pattern we can only use it now with WordDocumentUploader, so if we have other file type
+ * we will not be able to use this adapter pattern with them(other file type), we will need different adapter for each one 
+ * 
+ * */
 public class WordDocumentClassAdapter extends WordDocumentUploader implements DocumentUploader{
 
 	@Override
