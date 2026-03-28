0a266d472af44459037832ab0e1beb8e5ee50f12
sushantshinde023 <sushantshinde023@gmail.com>
2026-03-28 11:42:32 +0530

    feasible factory pattern with supplier

--- DesignPatterns/src/com/sushant/factory/method/pattern/TicketMachine.java
+++ DesignPatterns/src/com/sushant/factory/method/pattern/TicketMachine.java
@@ -1,15 +1,35 @@
 package com.sushant.factory.method.pattern;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
 public class TicketMachine {
 	
+//	public Ticket createTicket(String ticketType) {
+//		// TODO Auto-generated method stub
+//		if(ticketType.equalsIgnoreCase("Bus"))
+//			return new BusTicket();
+//		else
+//			return new TrainTickets();
+//		
+//	//The if else is ok for  2-3 types but let say we have some other implementation of tciket class as well. This will not feasible
+//	}
+	//Feasible solution
+	
+	private static final Map<String,Supplier<Ticket>> ticketSupplier=new HashMap<>();
+	
+	static {
+		ticketSupplier.put("Bus", BusTicket::new);
+		ticketSupplier.put("Train", TrainTickets::new);
+	}
 	public Ticket createTicket(String ticketType) {
-		// TODO Auto-generated method stub
-		if(ticketType.equalsIgnoreCase("Bus"))
-			return new BusTicket();
-		else
-			return new TrainTickets();
-		
-	//The if else is ok for  2-3 types but let say we have some other implementation of tciket class as well. This will not feasible
+		if(ticketSupplier.get(ticketType)!=null) {
+			return ticketSupplier.get(ticketType).get();
+		}
+		else {
+			throw new IllegalArgumentException("Invalid ticket type");
+		}
 	}
 
 }
