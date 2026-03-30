cb7562ceef81b262bb6a28b84d98d8df95a3f7fc
sushantshinde023 <sushantshinde023@gmail.com>
2026-03-30 16:33:52 +0530

    usecase

--- DesignPatterns/src/com/sushant/structural/proxy/pattern/MusicPlayer.java
+++ DesignPatterns/src/com/sushant/structural/proxy/pattern/MusicPlayer.java
@@ -4,23 +4,22 @@ public class MusicPlayer {
 	
 	public static void main(String[] args){
 		var user = new User("Jill");
+		var recommendations= new SongRecommendations(user);
 		
 		//Here for the time we are doing expensive operation that fetching songs from database
-		loadHomePage(user);
-		loadDiscoverPage(user);
+		loadHomePage(user,recommendations);
+		loadDiscoverPage(user,recommendations);
 		
 	}
-	private static void loadDiscoverPage(User user) {
+	private static void loadDiscoverPage(User user,SongRecommendations songRecommendations) {
 		System.out.println("Loading discover page ....");
-		var recommendations=new SongRecommendations(user);
-		recommendations.showRecommendations(user);
+		songRecommendations.showRecommendations(user);
 		
 	}
 
-	private static void loadHomePage(User user) {
+	private static void loadHomePage(User user,SongRecommendations songRecommendations) {
 		System.out.println("Loading Home page ...");
-		var recommendations= new SongRecommendations(user);
-		recommendations.showRecommendations(user);
+		songRecommendations.showRecommendations(user);
 		
 	}
 }
